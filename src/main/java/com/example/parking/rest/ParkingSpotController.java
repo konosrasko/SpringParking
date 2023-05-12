cbb21afec80b07fd8ddf9ad18c754b1c1a886778
Stamatis Chatzis <schatzis@ots.gr>
2023-05-12 15:54:34 +0300

    changes

--- src/main/java/com/example/parking/rest/ParkingSpotController.java
+++ src/main/java/com/example/parking/rest/ParkingSpotController.java
@@ -1,10 +1,7 @@
 package com.example.parking.rest;
 
 import com.example.parking.dto.ParkingSpotDTO;
-import com.example.parking.entity.ParkingSpot;
-import com.example.parking.repository.ParkingSpotRepo;
 import com.example.parking.service.ParkingSpotService;
-import com.example.parking.service.ParkingSpotServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,16 +15,16 @@ import java.util.List;
 public class ParkingSpotController {
 
     @Autowired
-    ParkingSpotService parkingSpotServiceImpl;
+    private ParkingSpotService parkingSpotService;
 
     @GetMapping("/parking-spots")
     public List<ParkingSpotDTO> getParkingZones(){
-        return parkingSpotServiceImpl.findAllParkingSpots();
+        return parkingSpotService.findAllParkingSpots();
     }
 
     @GetMapping("/parking-spot/{id}")
     public ParkingSpotDTO getParkingSpotsById(@PathVariable int id){
-        return parkingSpotServiceImpl.findParkingSpotById(id);
+        return parkingSpotService.findParkingSpotById(id);
     }
 
 }
