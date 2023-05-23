c5a259da6a192cfffb627d488f3f6b3c467eaad9
Stamatis Chatzis <schatzis@ots.gr>
2023-05-23 11:05:00 +0300

    pasok

--- src/main/java/com/example/parking/repository/ParkingZoneRepo.java
+++ src/main/java/com/example/parking/repository/ParkingZoneRepo.java
@@ -4,7 +4,6 @@ import com.example.parking.entity.ParkingZone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ParkingZoneRepo extends JpaRepository<ParkingZone, Integer> {
 
