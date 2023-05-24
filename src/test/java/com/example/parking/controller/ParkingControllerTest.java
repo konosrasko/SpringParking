83e0ff8cc91fb30d8db88d6ad807b451a006fa0d
Stamatis Chatzis <schatzis@ots.gr>
2023-05-24 15:46:39 +0300

    fix imports in test

--- src/test/java/com/example/parking/controller/ParkingControllerTest.java
+++ src/test/java/com/example/parking/controller/ParkingControllerTest.java
@@ -6,39 +6,28 @@ import com.example.parking.dto.ParkingZoneDTO;
 import com.example.parking.entity.Parking;
 import com.example.parking.entity.ParkingSpot;
 import com.example.parking.entity.ParkingZone;
-import com.example.parking.repository.ParkingSpotRepo;
 import com.example.parking.service.ParkingService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.nio.file.Paths.get;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.notNull;
-import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(controllers = ParkingController.class)
@@ -120,11 +109,17 @@ class ParkingControllerTest {
     }
     @Test
     public void parkingController_getParkings_ReturnResponseDTO() throws Exception{
+        List<ParkingSpotDTO> spotList = new ArrayList<>();
+        ParkingZoneDTO zoneDTO = new ParkingZoneDTO("ax", "nai");
+        List<ParkingZoneDTO> zonesDTO = new ArrayList<>();
+        zonesDTO.add(zoneDTO);
+        ParkingDTO parkingDTO1 = new ParkingDTO("get_park_test", zonesDTO);
+        List<ParkingDTO> parkingResponse = new ArrayList<>();
+        parkingResponse.add(parkingDTO1);
 
+        when(parkingService.findAllParkings()).thenReturn(parkingResponse);
 
-        when(parkingService.findParkingById(parkingDTO.getParkingId())).thenReturn(parkingDTO);
-
-        ResultActions response = mockMvc.perform((RequestBuilder) get("/api/parking/1"));
+        ResultActions response = mockMvc.perform(get("/api/parking").contentType(MediaType.APPLICATION_JSON));
 
         response.andExpect(MockMvcResultMatchers.status().isOk());
     }
