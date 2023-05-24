91b7f8b1783dec6515d8b2e26822f3246dc3eac2
kraskopoulos <kraskopoulos@ots.gr>
2023-05-24 11:12:52 +0300

    H2 serviceTest

--- src/test/java/com/example/parking/repository/ParkingZoneRepoTest.java
+++ src/test/java/com/example/parking/repository/ParkingZoneRepoTest.java
@@ -1,37 +1,44 @@
-//package com.example.parking.repository;
-//
-//import com.example.parking.entity.Parking;
-//import com.example.parking.entity.ParkingZone;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//@DataJpaTest
-//class ParkingZoneRepoTest {
-//
-//    @Autowired
-//    private ParkingZoneRepo parkingZoneRepo;
-//    @Autowired
-//    private ParkingRepo parkingRepo;
-//    @Test
-//    void findByParkingId() {
-//        Parking parking = new Parking(1,"test");
-//        ParkingZone parkingZone = new ParkingZone(1,"type","name");
-//        parking.setParkingZones(new ArrayList<>());
-//        parking.getParkingZones().add(parkingZone);
-//        parkingRepo.save(parking);
-//        parkingZoneRepo.save(parkingZone);
-//
-//        Parking p = parkingRepo.findById(parking.getId()).get();
-//        List<ParkingZone> pz = parkingZoneRepo.findByParkingId(p.getId());
-//
-//        assertTrue(pz.equals(p.getParkingZones()));
-//
-//
-//
-//    }
-//}
+package com.example.parking.repository;
+
+import com.example.parking.entity.ParkingZone;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ParkingZoneRepoTest {
+    @Autowired
+    private ParkingZoneRepo parkingZoneRepo;
+
+    @Test
+    void saveTest() {
+        ParkingZone parkingZone = new ParkingZone("type", "name");
+        ParkingZone savedZone = parkingZoneRepo.save(parkingZone);
+        assertAll(
+                "Grouped assertions for parking zone",
+                () -> assertEquals(parkingZone, savedZone, "entity should match return value"),
+                () -> assertNotNull(savedZone, "saved value should not be null")
+        );
+    }
+
+    @Test
+    void shouldReturnTheSelectedSpot() {
+        ParkingZone parkingZone1 = new ParkingZone("type", "name");
+        ParkingZone parkingZone2 = new ParkingZone("type", "name");
+        parkingZoneRepo.save(parkingZone1);
+        parkingZoneRepo.save(parkingZone2);
+        ParkingZone zone1 = parkingZoneRepo.findById(parkingZone1.getId()).get();
+        assertAll(
+                "Grouped assertions for parking zone",
+                () -> assertEquals(parkingZoneRepo.findAll().size(), 2, "should save 2 values"),
+                () -> assertNotNull(zone1, "should get if saved"),
+                () -> assertEquals(parkingZone1, zone1, "should have matching values"),
+                () -> assertNotEquals(parkingZone2, zone1, "should not have same values")
+        );
+    }
+}
