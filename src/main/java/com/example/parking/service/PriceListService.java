688fff3af1f7d92037f8ba57f49288348d64e23a
kraskopoulos <kraskopoulos@ots.gr>
2023-05-29 10:45:16 +0300

    PriceList

--- src/main/java/com/example/parking/service/PriceListService.java
+++ src/main/java/com/example/parking/service/PriceListService.java
@@ -1,9 +1,18 @@
 package com.example.parking.service;
 
+import com.example.parking.dto.PriceListDTO;
 import com.example.parking.dto.PriceScaleDTO;
 import com.example.parking.entity.PriceScale;
 import java.util.List;
 
 public interface PriceListService {
+    PriceListDTO addPriceList();
+    PriceListDTO getPriceList(int zoneId);
+
     PriceScaleDTO addPriceScales();
+
+    void deleteScale(int scaleId);
+    void deletePriceList(int priceListId);
+
+
 }
--- src/main/java/com/example/parking/service/impl/PriceListServiceImpl.java
+++ src/main/java/com/example/parking/service/impl/PriceListServiceImpl.java
@@ -1,12 +1,33 @@
 package com.example.parking.service.impl;
 
+import com.example.parking.dto.PriceListDTO;
 import com.example.parking.dto.PriceScaleDTO;
 import com.example.parking.service.PriceListService;
 
 public class PriceListServiceImpl implements PriceListService {
 
     @Override
+    public PriceListDTO addPriceList() {
+        return null;
+    }
+
+    @Override
+    public PriceListDTO getPriceList(int zoneId) {
+        return null;
+    }
+
+    @Override
     public PriceScaleDTO addPriceScales() {
         return null;
+    }
+
+    @Override
+    public void deleteScale(int scaleId) {
+
+    }
+
+    @Override
+    public void deletePriceList(int priceListId) {
+
     }
 }
